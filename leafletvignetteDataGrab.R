73f479dca2915b879de10ec6dcf6141d10499acb
jkr216 <jonathan.regenstein@rstudio.com>
2016-08-26 19:25:52 +0000

    refactored again

--- leafletvignetteDataGrab.R
+++ leafletvignetteDataGrab.R
@@ -3,16 +3,13 @@ library(devtools)
 library(macrodata)
 library(Quandl)
 library(rgdal)
-library(countrycode)
 library(sp)
-library(readr)
-
-
-load('sourceData.RDat')
+##library(readr)
+library(DT)
+library(data.table)
 
 ##where to find the world map
 ## "http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip"
-
 
 ### Create spatial vector object
 world <-readOGR(".", "ne_50m_admin_0_countries", verbose = FALSE)
@@ -22,37 +19,21 @@ world <-readOGR(".", "ne_50m_admin_0_countries", verbose = FALSE)
 ##in this case we will pull in world bank data
 Quandl.api_key("iz-ThHX9dpWweuxYW43a")
 
-##get GDP per capita data for all countries
+##get econ data for all countries
 ##serachQ is part of the macrodata package
 ##see here for more info: http://regisely.com/blog/macrodata/
-
-searchWWDIall <- searchQ("gdp per capita", country = "Brazil", frequency = "annual", 
-                         database = "WWDI", view = FALSE)
-
-searchWWDIall2 <- searchQ("exchange rate population growth consumer price index real interest rate", country = "Brazil", frequency = "annual", 
+  
+econIndicators <- searchQ("gdp per capita exchange rate population growth consumer price index real interest rate labor force", country = "Brazil", frequency = "annual", 
                          database = "WWDI")
 
-dataWWDIall<- requestQ(searchWWDIall, c(5,6))
+##select out the time series we want
+##note: this takes quite a while - 5 to 10 minutes
+econData <- requestQ(econIndicators, c(17, 3, 1, 10, 19, 6, 5))
+names(econData) <- c("GDP Per Capita", "GDP Per Capita Growth", "Real Interest Rate", 
+                     "Exchange Rate", "CPI", "Labor Force Part. Rate", "Labor Female")
 
-dataWWDIall2<- requestQ(searchWWDIall2, c(1,4,8,13,15))
-
-names(dataWWDIall) <- c("GDP/CapitaGrowth", "GDP/Capita")
-
-names(dataWWDIall2) <- c("RealInterestRate", "ExchangeRate", "CPI", "FemaleLaborPartRate", "TotalLaborPartRate")
-
-
-##split up the data into new objects, might not be necessary?
-WorldGDPPerCapita <- dataWWDIall2$`GDP/Capita`
-WorldGDPPerCapitaGrowth <- dataWWDIall$`GDP/CapitaGrowth`
-
-
-WorldRealInterestRate <- dataWWDIall2$`RealInterestRate`
-WorldExchangeRate <- dataWWDIall2$`ExchangeRate`
-WorldCPI <- dataWWDIall2$`CPI`
-WorldFemaleLaborPartRate <- dataWWDIall2$`FemaleLaborPartRate`
-WorldTotalLaborPartRate <- dataWWDIall2$`TotalLaborPartRate`
+test <- lapply(econData, function(x) {merge(x[, "USA"], all=T) } )
+testDataTable <- as.data.table(test)
 
 ### Save
-save(world, WorldGDPPerCapita, WorldGDPPerCapitaGrowth, dataWWDIall, dataWWDIall2, WorldRealInterestRate, 
-     WorldExchangeRate, WorldCPI, WorldFemaleLaborPartRate,
-     WorldTotalLaborPartRate, counties, ga_counties_DF, file = 'sourceData.RDat')
+save(world, econData, file = 'sourceData.RDat')
